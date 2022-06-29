638a9563568cd1de5a348a3091f24bb92ef2bb24
pavel-romanov-91 <101830399+pavel-romanov-91@users.noreply.github.com>
2022-06-29 22:02:18 +0300

    я

--- Forwardlist/DataContainers.cpp
+++ Forwardlist/DataContainers.cpp
@@ -1,8 +1,70 @@
 ﻿#include<iostream>
 using namespace std;
 
+#define tab "\t"
+
+class Element
+{
+	int Data;
+	Element* pNext;
+public:
+	Element(int Data, Element* pNext = nullptr) :Data(Data), pNext(pNext)
+	{
+		cout << "EConstructor:\t" << this << endl;
+	}
+	~Element()
+	{
+		cout << "EDestructor:\t" << this << endl;
+	}
+	friend class ForwardList;
+};
+
+class ForwardList
+{
+	Element* Head;
+public:
+	ForwardList()
+	{
+		Head = nullptr;
+		cout << "LConstractor:\t" << this << endl;
+	}
+	~ForwardList()
+	{
+		cout << "LDestructor:\t" << this << endl;
+	}
+
+		//				Adding Elements
+	void push_front(int Data)
+	{
+		//1)Создаем новый элемент
+		Element* New = new Element(Data);
+		//2)Присоединяем новый элемент к началу списка
+		New->pNext = Head;
+		//3)Говорим, что новый элемент является головой списка
+		Head = New;
+	}
+
+	//				Methods:
+	void print()const
+	{
+		Element* Temp = Head;
+		while (Temp)
+		{
+			cout << Temp << tab << Temp->Data << tab << Temp->pNext << endl;
+			Temp = Temp->pNext;		//Переходим на следующие элементы
+		}
+	}
+};
+
 void main()
 {
 	setlocale(LC_ALL, "");
-
+	int n;
+	cout << "Введите размер списка: "; cin >> n;
+	ForwardList list;			//Односвязный список
+	for (int i = 0; i < n; i++)
+	{
+		list.push_front(rand() % 100);
+	}
+	list.print();
 }
