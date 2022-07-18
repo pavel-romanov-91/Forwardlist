6716ac4092e73a47f7085b537bc37ab1cc6fa254
pavel-romanov-91 <101830399+pavel-romanov-91@users.noreply.github.com>
2022-07-18 10:39:26 +0300

    HomWork

---
+++ List_2/Source.cpp
@@ -0,0 +1,85 @@
+#include<iostream>
+using namespace std;
+
+#define tab "\t"
+
+class Forward_List
+{
+	class Element
+	{
+		int Data;
+		Element* pNext;
+		Element* pPrev;
+	public:
+		Element(int Data, Element* pNext = nullptr, Element* pPrev = nullptr) :
+			Data(Data), pNext(pNext), pPrev(pPrev)
+		{
+			cout << "EConstructor:\t" << this << endl;
+		}
+		~Element()
+		{
+			cout << "EDestructor:\t" << this << endl;
+		}
+		friend class Forward_List;
+	};
+	Element* Head;		//Значение элемента
+	Element* Tail;		//Адрес следующего элемента
+	unsigned int size;	//Адрес предыдущего элемента
+public:
+	Forward_List()
+	{
+		Head = Tail = nullptr;
+		size = 0;
+		cout << "LConstructor:\t" << this << endl;
+	}
+	~Forward_List()
+	{
+		while (Head);
+		cout << "LDestructor:\t" << this << endl;
+	}
+
+	//				Adding elements
+	void push_front(int Data)
+	{
+		if (Head == nullptr && Tail == nullptr)
+		{
+			Head = Tail = new Element(Data);
+			size++;
+			return;
+		}
+		Element* New = new Element(Data);
+		New->pNext = Head;
+		Head->pPrev = New;
+	}
+
+	//				Removing elements
+	void pop_front()
+	{
+		Element* erased = Head;
+		Head = Head->pNext;
+		delete Head->pPrev;
+		Head->pPrev = nullptr;
+		size--;
+	}
+
+	//				Methods
+	void print()const
+	{
+		for (Element* Temp = Head; Temp; Temp = Temp->pNext)
+			cout << Temp << tab << Temp->pPrev << tab << Temp->Data << tab << Temp->pNext << endl;
+	}
+	
+};
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	int n;
+	cout << "Введите размер списка: "; cin >> n;
+	Forward_List forward_list;
+	for (int i = 0; i < n; i++)
+	{
+		forward_list.push_front(rand() % 100);
+	}
+	forward_list.print();
+}
