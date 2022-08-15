e1d1620d0e22e170da557b5fee1ed7d064ed0086
pavel-romanov-91 <101830399+pavel-romanov-91@users.noreply.github.com>
2022-08-15 19:14:26 +0300

    Binary

---
+++ BinaryTree/Source.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using std::cin;
+using std::cout;
+using std::end;
+
+#define tab "\t"
+#define delimiter "\n-----------------------------\n"
+
+void main()
+{
+	setlocale(LC_ALL, "");
+
+}
